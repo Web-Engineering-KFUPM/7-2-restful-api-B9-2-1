91d3669b3745746b547a09a0f9d8131d69b81f20
B9-2 <s202260760@kfupm.edu.sa>
2026-04-15 11:59:02 +0300

    7-2-restful-api-B9-2-1

--- 7-2-restful-api/server/models/song.model.js
+++ 7-2-restful-api/server/models/song.model.js
@@ -2,4 +2,13 @@ import mongoose from "mongoose";
 
 // db schema
 
+const songSchema = new mongoose.Schema(
+    {
+    title: { type: String, required: true, trim: true},
+    artist: { type: String, required: true, trim: true},
+    year: { type: Number, min:1800, max:2100}
+    },
+    { timestaps: true}
+);
+
 export const Song = mongoose.model("Song", songSchema);
